a1835b1ddb9232a9679f44d36c642589c0e22d69
vitorvanacor <vitor_vanacor@hotmail.com>
2018-08-23 22:05:44 -0300

    Add catch.cpp (testing lib) and initial tests

--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
 etapa=1
 
 all:
-	flex scanner.l
+	flex --header-file=lex.yy.h scanner.l
 	gcc -c lex.yy.c
 	gcc -Wall main.c lex.yy.o -lfl -o etapa$(etapa)
 
 zip:
 	tar cvzf etapa$(etapa).tgz Makefile main.c tokens.h scanner.l
+
+test:
+	gcc test/main_test.cpp -c
+	gcc test/main_test.o test/scanner_test.cpp -o test/run_tests
 
 clean:
 	rm -f etapa* lex.yy.*
---
+++ test/scanner_test.cpp
@@ -0,0 +1,18 @@
+#include "catch.hpp"
+
+extern "C" {
+#include "../lex.yy.h"
+#include "../tokens.h"
+}
+
+TEST_CASE( "Reserved words", "[scanner]" )
+{
+    SECTION("Types") {
+        yy_scan_string("int float bool char string");
+        REQUIRE(yylex() == TK_PR_INT);
+        REQUIRE(yylex() == TK_PR_FLOAT);
+        REQUIRE(yylex() == TK_PR_BOOL);
+        REQUIRE(yylex() == TK_PR_CHAR);
+        REQUIRE(yylex() == TK_PR_STRING);
+    }
+}
