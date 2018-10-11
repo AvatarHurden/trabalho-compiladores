55e5d473606e1db4cd8ef630910640f4c9130f21
Arthur Giesel Vedana <agieselvedana@gmail.com>
2018-10-11 15:49:11 -0300

    Add typing rule for literals and variable

--- semantic.c
+++ semantic.c
@@ -6,6 +6,24 @@ int typecheck(Node* node, SymbolsTable* table, TypeNode* out) {
     case INT:
       out->kind = INT_T;
       return 0;
+    case FLOAT:
+      out->kind = FLOAT_T;
+      return 0;
+    case BOOL:
+      out->kind = BOOL_T;
+      return 0;
+    case CHAR:
+      out->kind = CHAR_T;
+      return 0;
+    case STRING:
+      out->kind = STRING_T;
+      return 0;
+    case VARIABLE: {
+      VariableNode var = node->value->var_node;
+      Symbol* s = getSymbol(table, var.identifier);
+      if (s == NULL) return ERR_UNDECLARED;
+      *out = *(s->type);
+      return 0; }
   }
 
   return 0;
