dd7df841a09583331e21e7e3f11b9831597f73ea
Arthur Giesel Vedana <agieselvedana@gmail.com>
2018-10-11 10:01:59 -0300

    Add first type checking

--- semantic.c
+++ semantic.c
@@ -1,1 +1,12 @@
 #include "semantic.h"
+
+int typecheck(Node* node, SymbolsTable* table, TypeNode* out) {
+
+  switch (node->type) {
+    case INT:
+      out->kind = INT_T;
+      return 0;
+  }
+
+  return 0;
+}
