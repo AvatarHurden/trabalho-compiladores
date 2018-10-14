20c4f0e5d5b619d4fc4e5f8fbde33a3ba0e54ce0
Arthur Giesel Vedana <agieselvedana@gmail.com>
2018-10-13 21:56:33 -0300

    Add type check for some binary expressions

--- semantic.c
+++ semantic.c
@@ -38,6 +38,31 @@ bool match(TypeNode* t1, TypeNode* t2) {
     return t1->kind == t2->kind;
 }
 
+TypeKind infer(TypeNode left, TypeNode right) {
+  if (left.kind == STRING_T ||
+      left.kind == CHAR_T ||
+      left.kind == CUSTOM_T ||
+      right.kind == STRING_T ||
+      right.kind == CHAR_T ||
+      right.kind == CUSTOM_T)
+      return -1;
+  if (left.kind == FLOAT_T || right.kind == FLOAT_T)
+    return FLOAT_T;
+  if (left.kind == INT_T || right.kind == INT_T)
+    return INT_T;
+  if (left.kind == BOOL_T || right.kind == BOOL_T)
+    return BOOL_T;
+  return -1;
+}
+
+TypeKind convert(TypeNode expected, TypeNode actual) {
+  if (match(&expected, &actual))
+    return expected.kind;
+  if (infer(expected, actual) != -1)
+    return expected.kind;
+  return -1;
+}
+
 int typecheck(Node* node, SymbolsTable* table, TypeNode* out) {
 
   switch (node->type) {
@@ -56,6 +81,65 @@ int typecheck(Node* node, SymbolsTable* table, TypeNode* out) {
     case STRING:
       out->kind = STRING_T;
       return 0;
+    case BIN_OP: {
+      BinOpNode bin = node->value->bin_op_node;
+
+      TypeNode left_type;
+      int left = typecheck(bin.left, table, &left_type);
+      if (left != 0) return left;
+
+      TypeNode right_type;
+      int right = typecheck(bin.right, table, &right_type);
+      if (right != 0) return right;
+
+      switch (bin.type) {
+        case ADD:
+        case SUBTRACT:
+        case MULTIPLY:
+        case DIVIDE:
+        case MODULO:
+        case POW: {
+          // Only numerical types are accepted
+          TypeKind kind = infer(left_type, right_type);
+          if (kind == -1) return ERR_WRONG_TYPE;
+          out->kind = kind;
+          return 0; }
+        case GREATER:
+        case LESS_THAN:
+        case GREATER_EQUAL:
+        case LESS_EQUAL:
+          // Check for numerical inference
+          if (infer(left_type, right_type) != -1) {
+            out->kind = BOOL_T;
+            return 0;
+          }
+          return ERR_WRONG_TYPE;
+        case EQUAL:
+        case NOT_EQUAL:
+          out->kind = BOOL_T;
+          // Check for exact matches
+          if (match(&left_type, &right_type))
+            return 0;
+          // Check for numerical inference
+          if (infer(left_type, right_type) != -1)
+            return 0;
+          return ERR_WRONG_TYPE;
+        case AND:
+        case OR:
+          if (left_type.kind == BOOL_T && right_type.kind == BOOL_T) {
+            out->kind = BOOL_T;
+            return 0;
+          }
+          return ERR_WRONG_TYPE;
+        case BIT_AND:
+        case BIT_OR:
+          return 0;
+        case BASH_PIPE:
+        case FORWARD_PIPE:
+          return 0;
+      }
+
+    }
     case VARIABLE: {
       VariableNode var = node->value->var_node;
       Symbol* s = getSymbol(table, var.identifier);
@@ -86,13 +170,16 @@ int typecheck(Node* node, SymbolsTable* table, TypeNode* out) {
     case RETURN: {
       ListNode ret = node->value->return_node;
 
-      int check = typecheck(ret.value, table, out);
+      TypeNode value_type;
+      int check = typecheck(ret.value, table, &value_type);
       if (check != 0) return check;
 
       Symbol* expected_return = getReturn(table);
-      if (match(out, expected_return->type))
+      TypeKind kind = convert(*expected_return->type, value_type);
+      if (kind != -1) {
+        out->kind = kind;
         return 0;
-      else
+      } else
         return ERR_WRONG_PAR_RETURN; }
     case BLOCK: {
       int check = typecheck(node->value->block_node.value, table, out);
